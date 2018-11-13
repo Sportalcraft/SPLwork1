6a25fd380c3039de97b4b466a5016c16afdd6726
pelegavrham <pelegav@post.bgu.ac.il>
2018-11-13 11:25:55 +0200

    commit

--- SPLwork1/Restaurant.cpp
+++ SPLwork1/Restaurant.cpp
@@ -1,20 +1,86 @@
 #include "Restaurant.h"
 #include "vector"
 #include "iostream"
+#include "fstream"
+#include "string"
+#include <stdexcept>
+#include <sstream>
+
 using namespace std;
+
 
 Restaurant::Restaurant()
 {
     open=true;
-    tables=new vector<Table*>;
-    menu= new vector<Dish>;
-    actionsLog= new vector<BaseAction*>;
+    vector<Table*> tables;
+    vector<Dish> menu;
+    vector<BaseAction*> actionsLog;
 }
 
 Restaurant ::Restaurant(const string &configFilePath)
 {
     open=true;
+    if(! ifs.is_open())
+        throw std::runtime_error("file not open");
+    ifs.open(configFilePath);
+    string s;
+    while (getline(ifs, s)){
+        if(s=="")
+            continue;
+        else {
+            if(s.equals("#Number of tables."){
+                while (getline(ifs, s)){
+                    if(s=="")
+                        continue;
+                    else {
+                        stringstream num(s);
+                        int x=0;
+                        num >> x;
+                        vector < Table * > tables(x);
+                        break;
+                    }
+                }
+            }
+            else if(s.equals("#Tables")){
+                while (getline(ifs, s)){
+                    if(s=="")
+                        continue;
+                    else {
+                        vector<int> vect;
+                        stringstream ss(s);
+                        int i;
+                        while (ss >> i)
+                        {
+                            vect.push_back(i);
+                            if (ss.peek() == ',')
+                                ss.ignore();
+                        }
+                    }
+                }
+            else if(s.equals("#Menu")){
+                    while (getline(ifs, s)){
+                        if(s=="")
+                            continue;
+                        else {
+                            vector<string> result;
+                            int i=0;
+                            while( s.good() )
+                            {
+                                string substr;
+                                getline( s, substr, ',' );
+                                result.push_back( substr );
+                            }
+                            stringstream num(result[2]);
+                            int x=0;
+                            num >> x;
 
+                            Dish d(i,result[0] , x , result[1]);
+                            menu.push_back(d);
+                            i++;
+                        }
+            }
+        }
+    }
 }
 const vector<BaseAction *> & Restaurant::getActionsLog() const
 {
@@ -30,11 +96,12 @@ int Restaurant::getNumOfTables() const
 }
 Table* Restaurant::getTable(int ind)
 {
-
+    return tables[ind];
 }
 void Restaurant::start()
 {
     cout<<"Restaurant is now open!"<<endl;
+
 }
 Restaurant::~Restaurant()
 {
