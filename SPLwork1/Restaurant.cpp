9e8ca572f3e922c46fdc89013070560b410e025a
pelegavraham <pelegav@post.bgu.ac.il>
2018-11-15 10:45:39 +0200

    start function open table

--- SPLwork1/Restaurant.cpp
+++ SPLwork1/Restaurant.cpp
@@ -139,7 +139,10 @@ Table* Restaurant::getTable(int ind)
 void Restaurant::start()
 {
     cout<<"Restaurant is now open!"<<endl;
-
+	for(int i=0; i<getNumOfTables();i++)
+	{
+		tables[i]->openTable();
+	}
 }
 Restaurant::~Restaurant()
 {
